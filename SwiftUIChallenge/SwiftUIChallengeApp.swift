830a28b05ed465635588cc75eb10041527b1d85f
nitinpatil2468 <nitin.patil@beatoapp.com>
2021-12-22 10:43:50 +0530

    Initial Commit

---
+++ SwiftUIChallenge/SwiftUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallengeApp.swift
+//  SwiftUIChallenge
+//
+//  Created by Nitin Patil on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
